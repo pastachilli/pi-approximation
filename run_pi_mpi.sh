b50d19faed01ffff9b55cd934b0951cd8562ce31
Gabe Postacchini <gpostacchini@gmail.com>
2025-02-23 23:45:58 -0600

    fixed typo and set slots properly in script

--- run_pi_mpi.sh
+++ run_pi_mpi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #$ -N pi_mpi_job
 #$ -cwd
-#$ -pe mpi 4          # Adjust the number of processes as needed
-#$ =q AMCS
+#$ -pe orte 4
+#$ -q AMCS
 #$ -l h_rt=00:10:00
 #$ -o pi_mpi_output.txt
 #$ -e pi_mpi_error.txt
